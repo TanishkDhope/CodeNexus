0b9639948ebcb9d2d6c9b1e206b8772e86d818e5
Tanishk Dhope <tanishkdhope@gmail.com>
2025-03-08 07:36:18 +0530

    added pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true, // Enable service worker in dev mode
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,svg}"], // Cache these files
+      },
+      manifest: {
+        name: "CodeNexus",
+        short_name: "CodeNex",
+        description: "A Progressive Web App built with Vite",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          ,
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
 })
